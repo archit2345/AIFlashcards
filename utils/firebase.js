5168d387744d2a0798b5604d1f7b71887295849b
Saadiyah <saadiyahasif6@gmail.com>
2024-08-17 22:41:53 +0400

    .

--- utils/firebase.js
+++ utils/firebase.js
@@ -6,15 +6,6 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD1QEmDRRIhHut6Fwy6kS260yz5aWp_iYE",
-  authDomain: "ace-cards-9610b.firebaseapp.com",
-  projectId: "ace-cards-9610b",
-  storageBucket: "ace-cards-9610b.appspot.com",
-  messagingSenderId: "278714058059",
-  appId: "1:278714058059:web:aa2ece37bf298da028d4ce",
-  measurementId: "G-8CTQTHGSY1"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
